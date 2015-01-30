f501b60e91c1ca7f032f04475c27be6f7ad5a1c2
Patrick Beeson <patrickbeeson@gmail.com>
2015-01-30 16:48:38 -0500

    Add initial test for brand detail.

--- enunciator/brands/tests.py
+++ enunciator/brands/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class BrandsTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_brand(self):
+        """Ensure we can get a brand"""
+        url = reverse('brand-detail')
+        client = Client()
+        response = client.get(url)
+        print(response.content)
