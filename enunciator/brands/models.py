fa90427bae1770bd61cbf6304e4b5533b2a31428
Patrick Beeson <patrickbeeson@gmail.com>
2015-01-30 17:27:55 -0500

    Add initial model and attributes.

--- enunciator/brands/models.py
+++ enunciator/brands/models.py
@@ -1,3 +1,40 @@
+import requests
+
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    """
+    A brand object
+    """
+    name = models.CharField(
+        max_length=200,
+        help_text='Limited to 200 characters',
+        default=''
+    )
+    slug = models.SlugField(
+        help_text='Populates from the name field',
+        default='',
+        unique=True
+    )
+    description = models.TextField(
+        default=''
+    )
+    website = models.URLField(
+        default=''
+    )
+    logo = models.ImageField(
+        upload_to='brands/logos',
+        default='',
+        help_text='Please use jpg (jpeg) or png files only'
+        # TODO add validator
+    )
+    vine_url = models.URLField(
+        default=''
+    )
+    video_url = models.URLField(
+        default=''
+    )
+    video_thumbnail_url = models.URLField(
+        default=''
+    )
