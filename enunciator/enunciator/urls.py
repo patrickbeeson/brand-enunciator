b5075e50217389f0b957370a23ac8811e1c874f9
Patrick Beeson <patrickbeeson@gmail.com>
2015-02-06 10:39:47 -0500

    Allow media to be served on dev.

--- enunciator/enunciator/urls.py
+++ enunciator/enunciator/urls.py
@@ -3,8 +3,12 @@ from rest_framework.authtoken.views import obtain_auth_token
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 from brands.urls import router
+
+admin.site.site_header = 'Brand Enunciator administration'
 
 urlpatterns = [
     url(r'^api/token/', obtain_auth_token, name='api-token'),
@@ -12,3 +16,5 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
